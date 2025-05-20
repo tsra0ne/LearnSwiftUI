b75283a9ec6fca0111d055c55cdf175603242f28
Sravan Goud <tsravangoud164@gmail.com>
2025-05-20 12:38:47 +0530

    added text

---
+++ LearnSwiftUI/TextBootcamp.swift
@@ -0,0 +1,33 @@
+//
+//  TextBootcamp.swift
+//  LearnSwiftUI
+//
+//  Created by Sravan Goud on 20/05/25.
+//
+
+import SwiftUI
+
+struct TextBootcamp: View {
+    var body: some View {
+        Text("Hello, World!".capitalized)
+//            .font(.body)
+//            .fontWeight(.semibold)
+//            .bold()
+//            .underline()
+//            .underline(true, color: .red)
+//            .italic()
+//            .strikethrough()
+//            .strikethrough(true, color: .blue)
+//            .font(.system(size: 24, weight: .semibold, design: .monospaced))
+//            .baselineOffset(10)
+//            .kerning(10)
+            .multilineTextAlignment(.leading)
+            .foregroundStyle(.red)
+            .frame(width: 200, height: 100, alignment: .leading)
+            .minimumScaleFactor(0.1)
+    }
+}
+
+#Preview {
+    TextBootcamp()
+}
