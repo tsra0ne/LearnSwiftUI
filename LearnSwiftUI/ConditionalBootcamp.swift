44c7223f69fbd8490577a5e8114e71023ae1ea13
Sravan Goud <tsravangoud164@gmail.com>
2025-05-25 17:59:34 +0530

    added conditional

---
+++ LearnSwiftUI/ConditionalBootcamp.swift
@@ -0,0 +1,55 @@
+//
+//  ConditionalBootcamp.swift
+//  LearnSwiftUI
+//
+//  Created by Sravan Goud on 25/05/25.
+//
+
+import SwiftUI
+
+struct ConditionalBootcamp: View {
+    
+    @State var showCircle: Bool = false
+    @State var showRectangle: Bool = false
+    @State var isLoading: Bool = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            Button("IS LOADING: \(isLoading.description)") {
+                isLoading.toggle()
+            }
+            
+            if isLoading {
+                ProgressView()
+            }
+            
+//            Button("Show Circle: \(showCircle.description)") {
+//                showCircle.toggle()
+//            }
+//            Button("Show Rectangle: \(showRectangle.description)") {
+//                showRectangle.toggle()
+//            }
+//            
+//            if showCircle {
+//                Circle()
+//                    .frame(width: 100, height: 100)
+//            }
+//            
+//            if showRectangle {
+//                Rectangle()
+//                    .frame(width: 100, height: 100)
+//            }
+//            
+//            if showCircle && showRectangle {
+//                RoundedRectangle(cornerRadius: 25)
+//                    .frame(width: 200, height: 100)
+//            }
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    ConditionalBootcamp()
+}
